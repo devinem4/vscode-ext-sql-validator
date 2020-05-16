d31f59adf327e7835a98e75a078d8c952b943e67
gammaflauge <mddevine@gmail.com>
2020-05-15 22:16:48 -0400

    :cake: First draft

--- src/extension.ts
+++ src/extension.ts
@@ -2,22 +2,45 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// https://github.com/lfittl/pg-query-emscripten
+const PgQuery = require('pg-query-emscripten');
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sql-validator" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('sql-validator.validateSql', () => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sql-validator!');
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('Could not find any SQL to validate.');
+			return
+		}
+
+		const selection = editor.document.getText(editor.selection); 
+		const sql = selection || editor.document.getText(); 
+		console.log(`input = ${sql}`);
+
+		let output = vscode.window.createOutputChannel('Validate SQL');
+
+		output.show(true);
+		output.appendLine("✨ SQL Validator ✨");
+		output.appendLine("-----------------\n");
+
+		const result = PgQuery.parse(sql);
+		if (result.error) {
+			const startLine = selection ? editor.selection.start.line : 0;
+			const sqlLinesBeforeError = sql.substring(0, result.error.cursorpos).match(/\n/g);
+			const errorLine = sqlLinesBeforeError ? sqlLinesBeforeError.length : 0 + startLine + 1;
+			output.appendLine(`Error found on line ${errorLine}\n`)
+			output.appendLine(result.error.message);
+		} else {
+			output.appendLine("🎂 looks good!")
+		}
 	});
 
 	context.subscriptions.push(disposable);
